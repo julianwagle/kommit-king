e3230935469787cf5b9a16be725fd438965f9bcb
julianwagle <julian.wagle@gmail.com>
2022-08-04 17:24:00 -0500

    20220804172400

--- kommit.sh
+++ kommit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Grab input variables
 while getopts u:t: flag
 do
     case "${flag}" in
@@ -8,6 +9,7 @@ do
     esac
 done
 
+# Set variables for the script
 GITHUB_ORIGIN="https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_USERNAME}/kommit-king.git"
 SCRIPT_DIR=$( dirname -- "$( readlink -f -- "$0"; )"; )
 FILE_PATH="$SCRIPT_DIR/kommit.sh"
@@ -19,7 +21,7 @@ CRONTAB=$(crontab -l)
 cd $SCRIPT_DIR
 if [[ "$CRONTAB" == *"$CRONJOB"* ]]; 
 then
-  echo "Cronjob already exists"
+  echo "Doing nothing (:"
 else
     sudo chmod +x $FILE_PATH
     gh repo create kommit-king --private
@@ -47,10 +49,5 @@ git push -u origin main --force
 # # # TIME BELOW  WILL CHANGE EVERY 10 MINUTES # # #
 ####################################################
 ####################################################
-# 20220804170700
-# 20220804171900
-# 20220804172000
-# 20220804172100
-# 20220804172200
-# 20220804172300
-# 20220804172300
+# 20220804172400
+# 20220804172400
