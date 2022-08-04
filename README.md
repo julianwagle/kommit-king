fb4f3e9709e2435df3849eb5f47d850373be96c0
julianwagle <julian.wagle@gmail.com>
2022-08-04 14:19:35 -0500

    20220804141935

---
+++ README.md
@@ -0,0 +1,19 @@
+Developed for use on MacOS - likely works on other Unix-based systems.
+
+No intention of supporting Windows any time soon.
+
+Pre-req install:
+[Github CLI](https://cli.github.com/)
+
+Quick start:
+
+```bash
+gh repo clone julianwagle/kommit-king
+cd kommit-king && bash kommit.sh <YOUR_GITHUB_USERNAME>
+```
+
+You will now have your github profile updated every ten minutes while your computer is open and connected to the internet.
+
+You will look more and more baller over the next year.
+
+![cook kid](https://media.giphy.com/media/xTiTngBQncyTMceuXK/giphy.gif)
---
+++ kommit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+GITHUB_USERNAME=$1
+FILE_PATH="$(pwd)/kommit.sh"
+RUN_EVERY='*/10 * * * *'
+CRONJOB="$RUN_EVERY $FILE_PATH $1"
+NOW=$(date +"%Y%m%d%H%M%S")
+echo $FILE_PATH
+CRONTAB=$(crontab -l)
+if [[ "$CRONTAB" == *"$CRONJOB"* ]]; 
+then
+  echo "Cronjob already exists"
+else
+    sudo chmod +x $FILE_PATH
+    gh repo create kommit-king --private
+    git init
+    crontab -l > mycron
+    echo "$CRONJOB">> mycron
+    crontab mycron
+    rm mycron
+fi
+
+sed -i '' -e '$ d' kommit.sh
+sh -c "echo '# $NOW' >> kommit.sh"
+
+git remote remove origin
+git remote add origin https://github.com/$GITHUB_USERNAME/kommit-king.git
+git add .
+git commit -m $NOW
+git branch -M main
+git push -u origin main
+
+# # I WILL CHANGE EVERY 10 MINUTES
+# 20220804141935
